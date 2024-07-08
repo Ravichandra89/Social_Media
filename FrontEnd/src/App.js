959e33f18341e61425a73705f43e6bb7005d4ce6
Ravi chandra <githubravi01@gmail.com>
2024-07-08 16:23:15 +0530

    Browser Router SetUp

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./Features/HomePage/Home";
+import LoginPage from "./Features/LoginPage/Login";
+import ProfilePage from "./Features/ProfilePage/Profile";
+import NavBar from "./Features/Navbar/Navbar";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme.js";
+import Login from "./Features/LoginPage/Login";
+import Home from "./Features/HomePage/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />}  />;
+      </Routes>
+    </BrowserRouter>
   );
 }
 
